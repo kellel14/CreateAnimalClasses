865c09aac74d414d36a9dab4d7ce7c478123d88d
kellel14 <kellel14@gmail.com>
2019-06-27 17:04:39 -0400

    intial commit; need dog/mouse classes

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+//This is the parent class. also called the super class
+//add own comments for better understanding
+public class Animal {
+    public Animal(){
+        System.out.println("A new animal has been created!");
+    }
+
+    public String sleep(){
+        return "An animal sleeps...";
+    }
+
+    public String eat(){
+        return "An animal eats...";
+    }
+}
